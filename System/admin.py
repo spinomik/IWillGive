28e9596f08bea8df42e3ac1072a964557deebf69
Mikołaj Majewski <spinomik@onet.pl>
2021-06-06 15:47:40 +0200

    dodano model instytucji, włączono panel administratora

--- System/admin.py
+++ System/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category,Institution
+
+admin.site.register(Category)
+admin.site.register(Institution)
 
 # Register your models here.
--- System/models.py
+++ System/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+INSTITUTION_CHOICE = [
+    ('F','fundacja'),
+    ('OP','Organizacja pozarządowa'),
+    ('ZL','Zbiórka lokalna')]
 class Category(models.Model):
     def __str__(self):
         return self.name
@@ -9,3 +13,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Kategoria"
         verbose_name_plural = "Kategorie"
+
+class Institution(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    type = models.CharField(max_length=2, choices=INSTITUTION_CHOICE, default='F')
+    categories = models.ManyToManyField(Category)
+
+    class Meta:
+        verbose_name = "Instytucja"
+        verbose_name_plural = "Instytucje"
+
